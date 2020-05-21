cc2da227da74f0ec900b519ffa9fe40b4c824f98
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-21 21:02:16 +0430

    Writing test for repositories against H2 in-memory database

--- P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
+++ P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
@@ -1,17 +1,29 @@
 package com.udacity.course3.reviews;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.udacity.course3.reviews.entity.Product;
+import com.udacity.course3.reviews.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest
 public class ReviewsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    ProductRepository productRepository;
 
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    void findProduct() {
+        Product product1 = new Product(1, "iphone", "apple");
+        Product product2 = new Product(2, "galaxy", "samsung");
+
+        assertEquals(product1, productRepository.findById(1).get());
+        assertEquals(product2, productRepository.findById(2).get());
+    }
 }
