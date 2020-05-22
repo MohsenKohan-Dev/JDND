1071112555660da3a515fa860c2db8582104a2a1
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-22 19:05:42 +0430

    Writing test for jpa and mongo repositories

--- P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
+++ P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
@@ -1,17 +1,12 @@
 package com.udacity.course3.reviews;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class ReviewsApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
