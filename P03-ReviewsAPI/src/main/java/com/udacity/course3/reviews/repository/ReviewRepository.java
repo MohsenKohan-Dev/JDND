0806790b96d42606de78434940b049239afca200
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-22 18:58:08 +0430

    Defining mongo and jpa repositories

---
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.udacity.course3.reviews.repository;
+
+import com.udacity.course3.reviews.entity.Review;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
+    List<Review> findAllByProductId(Integer id);
+}
